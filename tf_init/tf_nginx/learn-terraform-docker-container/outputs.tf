dfbddbc31131250f78703fe26f4dc7fc9c99e27d
pgp <pgpgpgp16@gmail.com>
2026-09-04 23:39:40 +0530

    Update 040926_233940

--- tf_init/tf_nginx/learn-terraform-docker-container/outputs.tf
+++ tf_init/tf_nginx/learn-terraform-docker-container/outputs.tf
@@ -7,3 +7,8 @@ output "image_id" {
   description = "ID of the Docker image"
   value       = docker_image.nginx.id
 }
+
+output "container_name" {
+  description = "Name of the Docker container"
+  value = docker_container.nginx.name
+}
