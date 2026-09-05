75a063b5b335115d44403dae32fd4ba5987041be
pgp <pgpgpgp16@gmail.com>
2026-09-05 11:13:26 +0530

    Update 050926_111326

--- tf_init/tf_proj/learn-terraform-resources/init-script.sh
+++ tf_init/tf_proj/learn-terraform-resources/init-script.sh
@@ -11,5 +11,4 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 cd /var/www/html
-curl http://169.254.169.254/latest/meta-data/instance-id -o index.html
 curl https://raw.githubusercontent.com/hashicorp/learn-terramino/master/index.php -O
