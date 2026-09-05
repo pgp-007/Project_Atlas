e9cdb5bff2ee3ae86806c858b74b73959c660f67
pgp <pgpgpgp16@gmail.com>
2026-09-05 08:36:40 +0530

    Update 050926_083640

--- tf_init/tf_proj/learn-terraform-resources/terraform.tf
+++ tf_init/tf_proj/learn-terraform-resources/terraform.tf
@@ -1,17 +1,4 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
-
-  /* Uncomment this block to use Terraform Cloud for this tutorial
-  cloud {
-      organization = "organization-name"
-      workspaces {
-        name = "learn-terraform-*"
-      }
-  }
-  */
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
