b35cc185c7335fb63e846135fb87f1105f3e3223
pgp <pgpgpgp16@gmail.com>
2026-09-05 12:50:28 +0530

    Update 050926_125028

--- tf_init/tf_proj/learn-terraform-resources/init-script.sh
+++ tf_init/tf_proj/learn-terraform-resources/init-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 dnf update -y
-dnf remove -y httpd httpd-tools
-dnf install -y httpd php php-mysqlnd mariadb105-server
+dnf install -y httpd php
 systemctl enable --now httpd
 usermod -a -G apache ec2-user
 chown -R ec2-user:apache /var/www
@@ -9,6 +8,5 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 cd /var/www/html
-TOKEN=$(curl -s -S -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-curl -s -S -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id -o index.html
-curl -s -S -O https://raw.githubusercontent.com/hashicorp/learn-terramino/master/index.php
+ec2-metadata -i > index.html
+curl -s -S -O https://raw.githubusercontent.com/pgp-007/aws/refs/heads/main/index.php
