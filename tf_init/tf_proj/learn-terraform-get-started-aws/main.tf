3e4dec8b060233d915c9070f13d4968b969cdfbe
pgp <pgpgpgp16@gmail.com>
2026-09-07 12:32:45 +0530

    Update 070926_123245

---
+++ tf_init/tf_proj/learn-terraform-get-started-aws/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "learn-terraform"
+  }
+}
