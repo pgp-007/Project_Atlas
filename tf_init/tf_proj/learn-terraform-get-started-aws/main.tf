54aa8c0ff9291e30bae0c4c0c51a5bdf00c1d89b
pgp <pgpgpgp16@gmail.com>
2026-09-09 05:28:28 +0530

    Update 090926_052828

--- tf_init/tf_proj/learn-terraform-get-started-aws/main.tf
+++ tf_init/tf_proj/learn-terraform-get-started-aws/main.tf
@@ -13,6 +13,6 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   tags = {
-    Name = "learn-terraform"
+    Name = "EC2-terraform"
   }
 }
